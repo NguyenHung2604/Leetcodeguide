78340c1f871abc1b25473f158bfab4989d2a3192
Nguyen Hung <nguyenshark379@gmail.com>
2025-04-23 08:26:12 +0700

    Reverse

---
+++ Integer to Roman.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+
+
+string intToRoman(int num) {
+    string ans;
+    int n[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string s[]= {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    
+    int i = 0;
+    while(i < 13)
+    {   
+        int turn = 0;
+        if(num >= n[i])
+        {
+            ans = ans + s[i];
+            num -= n[i];
+        }
+        else{
+        i++;
+        }
+    }
+    return ans;
+   
+    
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << intToRoman(n);
+    return 0;
+}
